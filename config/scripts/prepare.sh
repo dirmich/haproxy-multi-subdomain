9bd5006ed0e50e3ff97118981390caaf9496ef85
David, Shin <dirmich@gmail.com>
2021-05-07 18:14:17 +0900

    add for oldtv.cf kakaolab.ml

--- config/scripts/prepare.sh
+++ config/scripts/prepare.sh
@@ -1,3 +1,5 @@
-certbot --text --server https://acme-v02.api.letsencrypt.org/directory --authenticator certbot-pdns:auth certonly -d badat.ga -d *.badat.ga
+#certbot --text --server https://acme-v02.api.letsencrypt.org/directory --authenticator certbot-pdns:auth certonly -d badat.ga -d *.badat.ga
+certbot --text --server https://acme-v02.api.letsencrypt.org/directory --authenticator certbot-pdns:auth certonly -d oldtv.cf -d *.oldtv.cf
+certbot --text --server https://acme-v02.api.letsencrypt.org/directory --authenticator certbot-pdns:auth certonly -d kakaolab.ml -d *.kakaolab.ml
 
 certbot --text --server https://acme-v02.api.letsencrypt.org/directory --authenticator certbot-pdns:auth certonly -d lab.highmaru.com -d *.lab.highmaru.com
