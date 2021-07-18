8745475bfe113bfb106f8cbf4af1edfc59758ff4
David, Shin <dirmich@gmail.com>
2021-07-19 08:38:46 +0900

    fix for ws,wss

--- config/scripts/prepare.sh
+++ config/scripts/prepare.sh
@@ -1,5 +1,6 @@
-#certbot --text --server https://acme-v02.api.letsencrypt.org/directory --authenticator certbot-pdns:auth certonly -d badat.ga -d *.badat.ga
 certbot --text --server https://acme-v02.api.letsencrypt.org/directory --authenticator certbot-pdns:auth certonly -d oldtv.cf -d *.oldtv.cf
 certbot --text --server https://acme-v02.api.letsencrypt.org/directory --authenticator certbot-pdns:auth certonly -d kakaolab.ml -d *.kakaolab.ml
+certbot --text --server https://acme-v02.api.letsencrypt.org/directory --authenticator certbot-pdns:auth certonly -d arisoo.ml -d *.arisoo.ml
+certbot --text --server https://acme-v02.api.letsencrypt.org/directory --authenticator certbot-pdns:auth certonly -d margin.ga -d *.margin.ga
 
 certbot --text --server https://acme-v02.api.letsencrypt.org/directory --authenticator certbot-pdns:auth certonly -d lab.highmaru.com -d *.lab.highmaru.com
